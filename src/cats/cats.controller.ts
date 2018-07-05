9020fb599266d4768c2a9ae528ad081f185cff93
aseleznev <aa.seleznev@gmail.com>
2018-07-05 21:53:10 +0500

    add cats

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
+
+@Controller('cats')
+export class CatsController {
+  @Post()
+  create(@Body() createCatDto) {
+    return 'This action adds a new cat';
+  }
+
+  @Get()
+  findAll() {
+    return 'This action returns all cats';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id) {
+    return `This action returns a #${id} cat`;
+  }
+
+  @Put(':id')
+  update(@Param('id') id, @Body() updateCatDto) {
+    return `This action updates a #${id} cat`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id) {
+    return `This action removes a #${id} cat`;
+  }
+}
